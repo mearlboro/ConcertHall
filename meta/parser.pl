cc6aaa2a081247ee768b5c041cdc4c36b91f56f0
M <mearlboro@protonmail.com>
2020-06-01 23:29:21 +0100

    Update parser to process 3D acc data into JSON

--- meta/parser.pl
+++ meta/parser.pl
@@ -1,5 +1,30 @@
 #!/usr/bin/perl
 
+# uses the new logs that have been preprocessed in
+# github.com/mearlboro/ImprovisationSynchronization
+# and produces data for the web visualisation
+
+#  perl ConcertHall/meta/parser.pl 1
+# run in the parent folder of the two repos to output the
+# data where required, make sure it doesn't already exist
+# in ConcertHall/data
+
+#  .
+#  ├── ConcertHall
+#  │   ├── data
+#  │   ├── index.html
+#  │   ├── meta
+#  │   ├── Newlog_gen.py
+#  │   │   ├── parser.pl
+#  │   │   └── sensors_to_seats.csv
+#  │   ├── music
+#  │   ├── script.js
+#  │   └── styles.css
+#  └── ImprovisationSynchronization
+#      ├── Newlog_gen.py
+#      └── SVS_New_Logs
+
+
 use warnings;
 use strict;
 
@@ -16,36 +41,45 @@ my %timeframes = (
     8 => { start =>  1364, end =>  1509, },
 );
 
-my ($din, $dout) = ('./data', './out');
+my $sample_freq = 10;
+
+my ($din, $dout) = ('./ImprovisationSynchronization/SVS_New_Logs', './ConcertHall/data');
 my ($fin, $fout, $FIN, $FOUT);
 
 my ($piece) = @ARGV;
-my $seat;
+my ($sensor, $seat);
+
+my %sensors_to_seats = seats_sensors_csv();
+
+my $val_re = qr/([-.\d]+)/;
 
 printf "Processing piece %s\n", $piece;
 foreach $fin (glob("$din/*.csv")) {
 
-    if ($fin =~ /_(\w{3})[.]csv$/) {
-        $seat = $1;
+    if ($fin =~ /(SV\w{3})[.]csv$/) {
+        $sensor = $1;
+        $seat = $sensors_to_seats{$sensor};
 
-        printf "Processing file %s for seat %s", $fin, $seat;
+        if ($seat) {
+            printf "Processing file %s for device %s / seat %s", $fin, $sensor, $seat;
 
-        open $FIN, "<", $fin or die "Can't open '$fin'";
+            open $FIN, "<", $fin or die "Can't open '$fin'";
 
-        $fout = "$dout/$piece.js";
-        open $FOUT, ">>", $fout or die "Can't open '$fout'";
+            $fout = "$dout/$piece.js";
+            open $FOUT, ">>", $fout or die "Can't open '$fout'";
 
-        if (-z $fout) {
-        	if ($piece == 1) {
-        		print $FOUT "window.data = {};\n";
-        	}
-            print $FOUT "window.data[$piece] = {\n";
+            if (-z $fout) {
+                if ($piece == 1) {
+                    print $FOUT "window.data = {};\n";
+                }
+                print $FOUT "window.data[$piece] = {\n";
+            }
+
+            process_csv($seat, $piece, $FIN, $FOUT);
+
+            printf "\n";
+            close $FIN or die "Can't close '$fin'";
         }
-
-        process_csv($seat, $piece, $FIN, $FOUT);
-
-        printf "\n";
-        close $FIN or die "Can't close '$fin'";
     }
 }
 
@@ -54,40 +88,67 @@ close $FOUT or die "Can't close '$fout'";
 
 
 sub process_csv() {
-    my ($seat, $piece, $FIN, $FOUT) = @_;
+    my ($sensor, $piece, $FIN, $FOUT) = @_;
 
-    print $FOUT "\"$seat\" : {\n";
-    print $FOUT "  \"data\" : [ ";
+    print $FOUT "\"$sensor\" : {\n";
+    print $FOUT "  \"acc\" : [ ";
 
-    my ($sum, $vmin, $vmax, $count) = (0, 10000, 0, 0, 0);
-    my $timestamp;
+    my ($timestamp, $count) = (0, 0);
+	my ($x, $y, $z, $xsum, $ysum, $zsum) = (0, 0, 0, 0, 0, 0);
 
     while (<$FIN>) {
         chomp;
 
-        if ( /(-?\d+(?:[.]\d+)?),(\d+),(.*)/ ) {
+        if ( /([^,]+),$val_re,$val_re,$val_re/ ) {
 
-            $timestamp = $1 + 0;
-            if ($timestamp >= $timeframes{$piece}->{start} &&
-                $timestamp <= $timeframes{$piece}->{end}) {
+            if ($timestamp >= $sample_freq * $timeframes{$piece}->{start} &&
+                $timestamp <= $sample_freq * $timeframes{$piece}->{end}) {
 
-                my $value = $3 - 9.81;
-                $sum += $value;
-                $vmin = min ($vmin, $value);
-                $vmax = max ($vmax, $value);
-	            $count += 1;
+                $x = $2 + 0.0;
+                $y = $3 + 9.81;
+                $z = $4 + 0.0;
 
-                print $FOUT "$value, ";
+				$xsum += $x;
+				$ysum += $y;
+				$zsum += $z;
+
+                $count ++;
+
+                print $FOUT "[$x,$y,$z], ";
             }
-            elsif ($timestamp > $timeframes{$piece}->{end}) {
+            elsif ($timestamp > $sample_freq * $timeframes{$piece}->{end}) {
                 last;
             }
+
+            $timestamp ++;
         }
     }
     print $FOUT "],\n";
     print $FOUT "  \"count\" : " . $count . ",";
-    print $FOUT "  \"avg\" : " . $sum / $count . ",";
-    print $FOUT "  \"min\" : " . $vmin . ",";
-    print $FOUT "  \"max\" : " . $vmax . "\n";
+    if ($count != 0) {
+        print $FOUT "  \"avg\" : [" . $xsum / $count . "," . $ysum / $count . "," . $zsum / $count . "] ";
+    }
+    else {
+        print $FOUT "  \"avg\" : [ NaN, NaN, NaN ] \n";
+    }
+
     print $FOUT "},\n";
 }
+
+
+sub seats_sensors_csv() {
+    open my $FH, '<', "./ConcertHall/meta/sensors_to_seats.csv" or die "Can't open  'sensors_to_seats.csv'";
+
+    my %hash;
+
+    while (<$FH>) {
+        chomp;
+
+        if ( / (SV\d{3}) ; ([A-F]\d{2}) /ix ) {
+            $hash{$1} = $2;
+        }
+    }
+
+    return %hash;
+}
+
